4b96a391c81461305861ef49fa01d51f38ac707e
zamirguliyev <zamirquliyev7@gmail.com>
2022-07-31 17:12:09 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './components/Main';
+import './components/style.css'
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
